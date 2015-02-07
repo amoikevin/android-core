75f2dac812fb27ccec40521fbe246ffae4fa123c
snowdream <yanghui1986527@gmail.com>
2015-02-08 00:38:21 +0800

    Add : Fragment

---
+++ lib/src/main/java/com/github/snowdream/android/support/Fragment.java
@@ -0,0 +1,81 @@
+package com.github.snowdream.android.support;
+
+import android.app.Activity;
+import android.content.Context;
+
+import java.util.List;
+
+/**
+ * Created by hui.yang on 2015/2/7.
+ */
+public class Fragment extends android.support.v4.app.Fragment {
+
+    /**
+     * @return the context from the activity
+     */
+    final public Context getApplicationContext() {
+        Activity activity = getActivity();
+
+        if (activity == null) {
+            throw new IllegalStateException("Fragment " + this
+                    + " not attached to Activity");
+        }
+
+        return activity.getApplicationContext();
+    }
+
+    /**
+     * @return the context from the activity
+     */
+    final public Context getContext() {
+        Activity activity = getActivity();
+
+        if (activity == null) {
+            throw new IllegalStateException("Fragment " + this
+                    + " not attached to Activity");
+        }
+
+        return activity.getBaseContext();
+    }
+
+    /**
+     * The same as press the back key.
+     *
+     * @see android.support.v4.app.FragmentActivity#onBackPressed
+     */
+    final public void finishFragment() {
+        Activity activity = getActivity();
+
+        if (activity == null) {
+            throw new IllegalStateException("Fragment " + this
+                    + " not attached to Activity");
+        }
+
+        activity.onBackPressed();
+    }
+
+    /**
+     * close several fragment by step
+     *
+     * @param step the number of the fragments which will be finished.
+     *
+     */
+    final public void finishFragmentByStep(int step) {
+        Activity activity = getActivity();
+
+        if (activity == null) {
+            throw new IllegalStateException("Fragment " + this
+                    + " not attached to Activity");
+        }
+
+        List<android.support.v4.app.Fragment> list = getFragmentManager().getFragments();
+        if ( list == null || list.size() < step) {
+            throw new IllegalStateException("There is not enough Fragment to finish.");
+        }
+
+        for (int i = 0; i < step; i++) {
+            activity.onBackPressed();
+        }
+    }
+
+}
---
+++ lib/src/main/java/com/github/snowdream/android/support/FragmentActivity.java
@@ -0,0 +1,8 @@
+package com.github.snowdream.android.support;
+
+/**
+ * Created by hui.yang on 2015/2/7.
+ */
+public class FragmentActivity extends android.support.v4.app.FragmentActivity {
+
+}
