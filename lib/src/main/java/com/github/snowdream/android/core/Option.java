a5608284643bfbc40ba6b58e3a7820c69c3e9d84
snowdream <yanghui1986527@gmail.com>
2015-02-26 21:55:33 +0800

    Update: Option

--- lib/src/main/java/com/github/snowdream/android/core/Option.java
+++ lib/src/main/java/com/github/snowdream/android/core/Option.java
@@ -4,5 +4,8 @@ package com.github.snowdream.android.core;
  * Created by hui.yang on 2015/2/25.
  */
 public class Option {
-    public boolean verbose = Snowdream.isDebug();
+    /**
+     * explain what is being done
+     */
+    public boolean verbose = false;
 }
